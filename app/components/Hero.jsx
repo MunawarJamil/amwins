e3e9a0a58e1910c69a9f36f799096fc33351571a
Munawar Jamil <munawar.minhas777@gmail.com>
2025-02-21 15:51:21 +0500

    merged new branch

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -28,7 +28,7 @@ function Hero() {
               <p className="text-2xl font-bold text-sky-400">37,124</p>
               <p>Strategic Underwriter</p>
             </div>
-            <div className="border-l-2 border-sky-400 pl-2">
+            <div className="border-l-2  border-sky-400 pl-2">
               <p className="text-2xl font-bold text-sky-400">30,341</p>
               <p>Retail Agency Relationships</p>
             </div>
@@ -48,6 +48,7 @@ function Hero() {
         </div>
       </div>
 
+      {/* ================================================ */}
       {/* Desktop View */}
       <div className="relative header hidden lg:flex justify-center ">
         <Image
@@ -58,7 +59,9 @@ function Hero() {
           alt="image is missing"
         />
 
-        <div className="absolute flex top-1/3 pt-10 w-[70%] mx-auto gap-10 left-[50%] justify-evenly mt-14 text-white transform -translate-x-1/2 -translate-y-1/2">
+        <div className="absolute flex top-1/3 pt-10 w-[70%] font-[Asap] mx-auto gap-10 left-[50%] justify-evenly mt-14 text-white transform -translate-x-1/2 -translate-y-1/2">
+         
+         
           <div>
             <p className="text-gray-300 text-xl">
               Read Now: 2025 Market Outlook & Trends
@@ -68,40 +71,48 @@ function Hero() {
               <span className="text-white">You Ahead</span>
             </h1>
 
-            <div className="grid grid-cols-2 lg:grid-cols-3 w-[55%] mt-10 gap-y-16 gap-x-1 p-5">
-              <div className="border-l-2 border-gray-500 pl-4">
-                <p className="text-3xl font-bold text-white">$39B</p>
-                <p className="text-sm text-gray-300">
-                  Annual Premium Allocations
+            <div className="grid grid-cols-2 lg:grid-cols-3 gap-y-16 gap-x-1 p-5">
+              {/* Stats Grid */}
+              <div className="border-l-2 border-sky-400 pl-2">
+                <p className="text-2xl font-bold text-sky-400 border-b-2 border-gray-400 pb-2 inline-block">
+                  $39B
                 </p>
+                <p>Annual Premium Allocations</p>
               </div>
-              <div className="border-l-2 border-gray-500 pl-4">
-                <p className="text-3xl font-bold text-white">37,124</p>
-                <p className="text-sm text-gray-300">Strategic Underwriter</p>
-              </div>
-              <div className="border-l-2 border-gray-500 ml-7 pl-4">
-                <p className="text-3xl font-bold text-white">30,341</p>
-                <p className="text-sm text-gray-300">
-                  Retail Agency Relationships
+              <div className="border-l-2  border-sky-400 pl-2">
+                <p className="text-2xl font-bold text-sky-400 border-b-2 border-gray-400 pb-2 inline-block">
+                  37,124
                 </p>
+                <p>Strategic Underwriter</p>
               </div>
-              <div className="border-l-2 border-gray-500 pl-4">
-                <p className="text-3xl font-bold text-white">7,325</p>
-                <p className="text-sm text-gray-300">
-                  Global Workforce Presence
+              <div className="border-l-2 border-sky-400 pl-2">
+                <p className="text-2xl font-bold text-sky-400 border-b-2 border-gray-400 pb-2 inline-block">
+                  30,341
                 </p>
+                <p>Retail Agency Relationships</p>
               </div>
-              <div className="border-l-2 border-gray-500 pl-4">
-                <p className="text-3xl font-bold text-white">1,514</p>
-                <p className="text-sm text-gray-300">Employee Stakeholders</p>
-              </div>
-              <div className="border-l-2 border-gray-500 pl-4">
-                <p className="text-3xl font-bold text-white">30,341</p>
-                <p className="text-sm text-gray-300">
-                  Annual Submission Volume
+              <div className="border-l-2 border-sky-400 pl-2">
+                <p className="text-2xl font-bold text-sky-400 border-b-2 border-gray-400 pb-2 inline-block">
+                  7,325
                 </p>
+                <p>Global Workforce Presence</p>
+              </div>
+              <div className="border-l-2 border-sky-400 pl-2">
+                <p className="text-2xl font-bold text-sky-400 border-b-2 border-gray-400 pb-2 inline-block">
+                  1,514
+                </p>
+                <p>Employee Stakeholders</p>
+              </div>
+              <div className="border-l-2 border-sky-400 pl-2">
+                <p className="text-2xl font-bold text-sky-400 border-b-2 border-gray-400 pb-2 inline-block">
+                  30,341
+                </p>
+                <p>Annual Submission Volume</p>
               </div>
             </div>
+
+
+            
           </div>
 
           <div className="relative   h-auto">
@@ -122,9 +133,11 @@ function Hero() {
 
             <div className="absolute bottom-20 left-[-40] font-[Onest] bg-white rounded-lg p-4 shadow-lg max-w-xs">
               <p className=" relative text-sm  flex gap-3  items-center font-semibold text-gray-700">
-                <span className="lg:whitespace-nowrap  text-2xl font-bold ">Yearly Growth</span>
+                <span className="lg:whitespace-nowrap  text-2xl font-bold ">
+                  Yearly Growth
+                </span>
                 <div className="bg-black w-7 h-7 rounded-md flex items-center justify-center">
-                  <img src="/arrow-up.png" alt="arrow" className=" w-5 h-5 " />
+                  <img src="/arrow-up.png" alt="arrow" className="  " />
                 </div>
               </p>
               <p className="text-sm text-gray-600 mt-3">
